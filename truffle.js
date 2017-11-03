76480164bb3a0e69a02af88409add4f583481528
Andy Chen <amazingandyyy@gmail.com>
2017-11-03 16:05:03 -0700

    add rinkeby

--- truffle.js
+++ truffle.js
@@ -5,6 +5,12 @@ module.exports = {
       port: 8545,
       network_id: "*", // Match any network id
       gas: 4700000
+    },
+    rinkeby: {
+      host: 'localhost',
+      port: 8545,
+      network_id: 4,
+      gas: 4700000
     }
   }
 };
