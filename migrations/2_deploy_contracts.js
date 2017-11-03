c039df038579e3361a0b32eaf4fab5e0cc990a4a
Andy Chen <amazingandyyy@gmail.com>
2017-11-03 14:25:35 -0700

    [update] scripts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,30 @@
 const VentureCoinCrowdsale = artifacts.require("./VentureCoinCrowdsale.sol")
 
 module.exports = function(deployer, network, accounts) {
-  const startTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 1 // one second in the future
-  const endTime = startTime + (86400 * 20) // 20 days
-  const rate = new web3.BigNumber(1)
-  const wallet = accounts[0]
-
+  // const startTime =  latestTime() + duration.days(3);  // + 3 days
+  const startTime =  latestTime() + duration.seconds(1); // + immediately
+  const endTime = startTime + duration.days(30); // + 30 days
+  const rate = new web3.BigNumber(12);
+  const wallet = accounts[0];
+  
+  // deploy it here
   deployer.deploy(VentureCoinCrowdsale, startTime, endTime, rate, wallet)
+  .then( async () => {
+    const instance = await VentureCoinCrowdsale.deployed();
+    const token = await instance.token.call();
+    console.log('-----> VentureCoin(VCN) Address', token);
+  })
+};
+
+function latestTime() {
+  return web3.eth.getBlock('latest').timestamp;
+}
+
+const duration = {
+  seconds: function(val) { return val },
+  minutes: function(val) { return val * this.seconds(60) },
+  hours:   function(val) { return val * this.minutes(60) },
+  days:    function(val) { return val * this.hours(24) },
+  weeks:   function(val) { return val * this.days(7) },
+  years:   function(val) { return val * this.days(365)} 
 };
